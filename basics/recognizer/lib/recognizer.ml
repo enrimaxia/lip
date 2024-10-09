79fd142351dd8846c596235c7f64ab63d81f55ce
Docker <docker@example.com>
2024-10-09 15:19:47 +0100

    non funziona un cazzo xd (test malriuscito ma vabbeh si sbura)

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,14 +1,23 @@
-let lang1 _ = failwith ""
+type word = char list
+let string_of_charlist (lst : string) : word = lst |> String.to_seq |> List.of_seq
 
-let lang2 _ = failwith ""
+let lang1 : word -> bool = failwith ""
 
-let lang3 _ = failwith ""
+let rec lang1 (w : char list) : bool = match w with
+	| [] -> false
+	| '0' :: ls -> lang1 ls
+	| '1' :: ls -> lang1 ls
+	| _ -> false
 
-let lang4 _ = failwith ""
+let lang2 : word -> bool = failwith ""
 
-let lang5 _ = failwith ""
+let lang3 : word -> bool = failwith ""
+
+let lang4 : word -> bool = failwith ""
+
+let lang5 : word -> bool = failwith ""
     
 let recognizers = [lang1;lang2;lang3;lang4;lang5]
-                  
+
 let belongsTo w = List.map (fun f -> f w) recognizers
   
